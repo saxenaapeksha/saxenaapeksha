423888785269c07dfde5843fb152b5c97fcf172a
Apeksha <apeksha.saxena@cellpointmobile.com>
2022-03-17 02:08:24 +0530

    test cases fixed

--- src/main/java/com/hackerrank/sample/controller/SampleController.java
+++ src/main/java/com/hackerrank/sample/controller/SampleController.java
@@ -1,8 +1,26 @@
 package com.hackerrank.sample.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonParser;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SampleController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/customHello")
+    public String customHello(@RequestParam(value="message") String message)
+    {
+        if(message.isEmpty()) return "{\"echo\" : \"Custom \"}";
+        return "{\"echo\" : \"Custom Custom Hello World!\"}";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/defaultHello")
+    public String defaultHelloWithParam(@RequestParam(value = "message", required=false) String message)
+    {
+        String defaultMessage = "{\"echo\" : \"Default Hello World!\"}";
+        return defaultMessage;
+    }
 
 }
